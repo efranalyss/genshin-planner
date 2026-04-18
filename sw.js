2b58816b844fbfe06c6e17cf1a0f651647e3064b
Kate <mpkate71@gmail.com>
2026-04-19 00:54:39 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'gw-planner-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Cinzel:wght@400;600;700;900&family=Quicksand:wght@400;500;600;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
